28f784a841942455b2958f68f08ba0eb69d953e2
EC2 Default User <ec2-user@ip-172-31-22-60.ec2.internal>
2023-01-11 04:38:51 +0000

    アプリケーションを作ってみよう課題

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'top' => 'homes#homes'
+  resources :books
+  get 'new' => 'books#new', as: 'new'
+  #post 'books' => 'books#create'
+  #get 'books' => 'books#index'
+  get 'books/:id' => 'books#show', as: 'list'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_list'
+  patch 'books/:id' => 'books#update', as: 'update_list'
+  delete 'books/:id' => 'books#destroy' , as: 'destroy_list'
+  root to: 'homes#homes'
+end
